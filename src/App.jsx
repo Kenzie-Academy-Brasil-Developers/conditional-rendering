40e7c1be6518385b64272443c5219898ec5cf679
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-07-19 15:09:28 -0300

    contional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import { Example } from "./components/Example";
+import "./style.css";
+
 function App() { 
+  const [isActive, setIsActive] = useState(false);
+  // condição ? resposta verdadeira : resposta falsa (if/else)
+
   return (
     <div className="App">
+      <button onClick={() => setIsActive(!isActive)}>
+        {isActive ? "Desativar" : "Ativar"}
+      </button>
+      <Example isActive={isActive} />
      
+      <div className={isActive ? "active" : ""}>
+        Exemplo
+      </div>
     </div>
   )
 }
